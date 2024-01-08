ee08d92a6dc7e6929d3117a2022490d0a1c34c82
Anu Shree Gupta <89857101+anushree104120@users.noreply.github.com>
2024-01-08 11:27:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import TopNav from './components/TopNav';
+import Wise from './components/Wise';
+import Unlock from './components/Unlock';
+import Mode from './components/Mode';
+import Footer from './components/Footer';
+import Counting from './components/Counting';
+import Feacture from './components/Feacture';
+import HeroSection from './components/HeroSection';
+import Add from './components/Add';
+import Feature from './components/Feature';
+
+
+function App() {
+  return (
+
+    <>
+      <TopNav/>
+      <Navbar/>
+      <HeroSection/>
+      <Add/>
+      <Feature/>
+      <Feacture/>
+      <Counting/>
+      <Wise/>
+      <Unlock/>
+      <Mode/>
+      <Footer/>
+      
+    </>
+   
+  );
+}
+
+export default App;
